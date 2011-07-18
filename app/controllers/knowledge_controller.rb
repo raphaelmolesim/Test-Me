d9ca60d5e11b1847e19ece30bd8c9c8ab3461ff3
Raphael Molesim <raphael.sm86@gmail.com>
2011-07-18 19:54:18 +0100

    Added log message

--- app/controllers/knowledge_controller.rb
+++ app/controllers/knowledge_controller.rb
@@ -7,7 +7,7 @@ class KnowledgeController < ApplicationController
 
   def upload
     file = File.read(params["file"].path)
-    puts file.class
+    puts file.enconding.name
     name = params["name"]
     @knowledge = Knowledge.create_based_on_file(file, name, current_user.id) 
     render :action => "show"
